d5a8eef0d34ffda1cf7b55a07bbf33eec1e8b6ff
narutard15 <sid15dharthm@gmail.com>
2020-03-14 23:04:24 +0530

    small device responsive fix

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -5,109 +5,111 @@ import Videotile from '../Videotile/Videotile';
 function Body() {
   return (
     <>
-      <div className="app">
-        <ul className="hs full no-scrollbar">
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-        </ul>
-      </div>
+      <div className="is-full-screen">
+        <div className="app">
+          <ul className="hs full no-scrollbar">
+            <li className="item col-3">
+              <Videotile />
+            </li>
+            <li className="item col-3">
+              <Videotile />
+            </li>
+            <li className="item col-3">
+              <Videotile />
+            </li>
+            <li className="item col-3">
+              <Videotile />
+            </li>
+            <li className="item col-3">
+              <Videotile />
+            </li>
+            <li className="item col-3">
+              <Videotile />
+            </li>
+            <li className="item col-3">
+              <Videotile />
+            </li>
+            <li className="item col-3">
+              <Videotile />
+            </li>
+            <li className="item col-3">
+              <Videotile />
+            </li>
+            <li className="item col-3">
+              <Videotile />
+            </li>
+          </ul>
+        </div>
 
-      <div className="app">
-        <ul className="hs full no-scrollbar">
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-        </ul>
-      </div>
+        <div className="app">
+          <ul className="hs full no-scrollbar">
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+          </ul>
+        </div>
 
-      <div className="app">
-        <ul className="hs full no-scrollbar">
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-          <li className="item">
-            <Videotile />
-          </li>
-        </ul>
+        <div className="app">
+          <ul className="hs full no-scrollbar">
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+            <li className="item">
+              <Videotile />
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
